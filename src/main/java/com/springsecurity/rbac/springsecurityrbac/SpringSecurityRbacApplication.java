b875cdbd5d2c631df4171f735ec2829b248cc88a
Amit Chavda <amitchavda9879@gmail.com>
2022-08-05 09:17:26 +0530

    Initial commit

---
+++ src/main/java/com/springsecurity/rbac/springsecurityrbac/SpringSecurityRbacApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.rbac.springsecurityrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/rbac/springsecurityrbac/SpringSecurityRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.rbac.springsecurityrbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
